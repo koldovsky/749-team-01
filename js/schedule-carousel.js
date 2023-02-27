1132de473d0ead8b6ad23c8e49e9e95127d6c0b5
Andy Dorokhin <35396894+AndyDorokhin@users.noreply.github.com>
2023-02-27 21:27:02 +0000

    positioning  schedule items

---
+++ js/schedule-carousel.js
@@ -0,0 +1,28 @@
+const scheduleSwiper = new Swiper('.schedule-daily-container',{
+    loop: true,
+    navigation: {
+        nextEl: '.schedule-slider-next',
+        prevEl: '.schedule-slider-prev',
+      },
+      simulateTouch: true,
+      touchRadio: 1,
+      touchAngle: 45,
+      grabCursor: true,
+      autoHeight: true,
+      spaceBetween: 10,
+      watchOverflow: true,
+      breakpoints:{
+        480:{
+            slidesPerView: 1,
+        },
+        480:{
+            slidesPerView: 2,
+        },
+        770:{
+            slidesPerView:3
+        },
+        1000:{
+            slidesPerView:6
+        },
+      }
+});
