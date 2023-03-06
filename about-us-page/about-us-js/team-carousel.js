ec9a6e2b02f2eee3682041d0ed946bd0a33abc29
unknown <ivshk..554411@gmail.com>
2023-03-06 09:49:53 +0200

    Fix carousel team about us

--- about-us-page/about-us-js/team-carousel.js
+++ about-us-page/about-us-js/team-carousel.js
@@ -4,5 +4,20 @@ new Swiper('.swiper', {
     nextEL: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
+  simulateTouch: true,
+      touchRadio: 1,
+      touchAngle: 45,
+      grabCursor: true,
+      autoHeight: true,
+      spaceBetween: 10,
+      watchOverflow: true,
+      breakpoints:{
+        768:{
+            slidesPerView: 2,
+        },
+        996:{
+            slidesPerView:6
+        },
+      }
 });
 })();
