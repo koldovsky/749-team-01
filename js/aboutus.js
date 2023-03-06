b878578c8bc0787c4c279ce33b4a8b8b53a48d4b
andriimarmarosovych <andrewmartsyniuk@gmail.com>
2023-03-07 01:51:22 +0200

    changed About Us JS code

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,12 +1,12 @@
-const pageTitle = document.querySelector('.aboutus__subtitle');
-const colors = ['yellow', 'blue'];
-
-let colorIndex = 0;
-
 function changeColor() {
+    const pageTitle = document.querySelector('.aboutus__subtitle');
+    const colors = ['pink', 'blue'];
+    let colorIndex = 0;
   
-  pageTitle.style.color = colors[colorIndex];
-
-  colorIndex = (colorIndex + 1) % colors.length;
-}
-setInterval(changeColor, 3000);
+    setInterval(function() {
+      pageTitle.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+    }, 3000);
+  }
+  
+  changeColor();
