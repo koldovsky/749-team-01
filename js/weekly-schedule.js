e81ca451dcc0331f563caba379941a86a3ee9887
Andy Dorokhin <35396894+AndyDorokhin@users.noreply.github.com>
2023-03-08 10:51:09 +0000

    fixing schedule section

---
+++ js/weekly-schedule.js
@@ -0,0 +1,10 @@
+/*
+const contacts = ['+ 1 (234) 567 89 00', '0 800 501 482', 'SPICY'];
+const scheduleInfoPhoneSection = document.querySelector('.schedule-info-phone-number');
+let currentElement = 0;
+
+setInterval(() => {
+  scheduleInfoPhoneSection.textContent = contacts[currentElement];
+  currentElement = (currentElement++) % contacts.length;
+}, 1500);
+*/
