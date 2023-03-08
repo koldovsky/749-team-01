e0ec1035ec9ddf9ac9e39d1363fa0c2caee8f872
blo0dy66 <andriyprotsiv76@gmail.com>
2023-03-08 16:03:38 +0200

    finally fixed margin, img size etc

--- js/conctacts-how-to-contact.js
+++ js/conctacts-how-to-contact.js
@@ -1,15 +1,13 @@
-(function() {
+(function () {
     const headerContactsItems = document.querySelectorAll('.header__contacts-items');
-  
+
     headerContactsItems.forEach(item => {
-      item.addEventListener('mouseover', () => {
-        // code to add a hover effect, for example:
-        item.style.backgroundColor = '#f2f2f2';
-      });
-  
-      item.addEventListener('mouseout', () => {
-        // code to remove the hover effect, for example:
-        item.style.backgroundColor = '';
-      });
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = '#f2f2f2';
+        });
+
+        item.addEventListener('mouseout', () => {
+            item.style.backgroundColor = '';
+        });
     });
-  })();
+})();
